f58fb6abf49d64379325ef08a78273c550a4d1d7
Angel-TheDev <angelantoinetted@yahoo.com>
2019-05-17 14:20:19 -0400

    added menu options

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -9,7 +9,27 @@ namespace VirtualPet
     class Program
     {
         static void Main(string[] args)
+
         {
+            string response = "";
+
+                Console.WriteLine("Please choose one of the following menu options.");
+            Console.WriteLine("Type play or exit");
+            response = Console.ReadLine().ToLower();
+
+            
+            switch (response)
+
+            {
+                case "play":
+                    break;
+                case "exit":
+                    Console.WriteLine("Thank you for playing!");
+                    break;
+
+
+            }
+
             
         }
     }
