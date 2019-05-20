41ce298c394279c053b94ee92045747fdde7db42
Blair <bgriffit@oberlin.edu>
2019-05-20 15:27:48 -0400

    Started cleanup

--- VirtualPet/Menu.cs
+++ VirtualPet/Menu.cs
@@ -4,16 +4,38 @@ using System.Text;
 
 namespace VirtualPet
 {
-    static class Menu
+    class Menu
     {
-        static public void GameplayMenu()
+        VirtualPet first = new VirtualPet("", "");
+        public void StartMenu()
         {
-            {
+            string response = "";
+
+                Console.WriteLine("Please choose one of the following menu options.");
+                Console.WriteLine("Type play or exit");
+                response = Console.ReadLine().ToLower();
+
+                switch (response)
+                {
+                    case "play":
+                        Console.WriteLine("Let's name your first pet.");
+                        first.Name = Console.ReadLine();
+                        Console.WriteLine($"{first.Name} is a great name! What species is {first.Name}?");
+                        first.Species = Console.ReadLine();
+                        GameplayMenu();
+                        break;
+                    case "exit":
+                        Console.WriteLine("Thank you for playing!");
+                        break;
+                }
+        }
+        public void GameplayMenu()
+        {
             
             switch (response)
             {
                 case "pet info":
-                    VirtualPet.GetInfo();
+                    GetInfo();
                     break;
                 case "pet status":
                     VirtualPet.GetStatus();
